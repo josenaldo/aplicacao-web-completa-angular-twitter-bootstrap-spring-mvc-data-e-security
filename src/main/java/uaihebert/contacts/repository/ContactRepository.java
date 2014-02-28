9542960cfdaa0f0193202a8400ec6a9adcaa074c
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2014-02-28 02:11:31 -0300

    Josenaldo do futuro - É sério que tu largou a menina esperando no quarto pra vir fazer esse commit?
    EU - Mas se eu não fizer, amanhã eu não posso estudar meio dia!
    
    Josenaldo do futuro - Put a keep are you! Nerdice tem limites! Volta pro
    quarto agora! Eu lembro bem que você tem bons motivos pra isso!
    
    Eu - E tenho mesmo! HAHAHAH Despedida de casado :P De novo kkkk

--- src/main/java/uaihebert/contacts/repository/ContactRepository.java
+++ src/main/java/uaihebert/contacts/repository/ContactRepository.java
@@ -3,9 +3,11 @@ package uaihebert.contacts.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import uaihebert.contacts.model.Contact;
 
+@Repository
 public interface ContactRepository extends
 		PagingAndSortingRepository<Contact, Integer> {
 	Page<Contact> findByNameLike(Pageable pageable, String name);
--- src/main/java/uaihebert/contacts/repository/UserRepository.java
+++ src/main/java/uaihebert/contacts/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package uaihebert.contacts.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import uaihebert.contacts.model.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 	User findByEmail(String email);
 }
